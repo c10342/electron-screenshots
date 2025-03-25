382b01b09db44fb1a31bafc432d49aee92dea4bb
c10342 <1376023740@qq.com>
2025-03-25 21:25:49 +0800

    fix(electron-screenshots): 修复打包报错

---
+++ packages/electron-screenshots/src/global.d.ts
@@ -0,0 +1,6 @@
+declare module '@jimp/plugin-print/load-font' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: any;
+  export default component;
+  export const loadFont:any;
+}
